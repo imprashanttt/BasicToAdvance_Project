3be70ba4fc9fbb84ffc47b5f3100056bffb66e7a
imprashanttt <gm932777@gmail.com>
2025-06-30 17:55:03 +0530

    first commit

--- contactBook.py
+++ contactBook.py
@@ -17,8 +17,39 @@ def contactBook():
             )
             contact[name] = number
         else:
-            for person, number in contact.items():
-                print(f"{person}:{number}")
+            choice=input("Do you want to update,delete or show the contacts or search contact(update,delete,show,search):- ").lower()
+            if choice=='update':
+                name = input("Enter the person name:- ").capitalize()
+                number = input(
+                    "Enter the user number (please write in this format(+countrycode number)):- "
+                    )
+                if name in contact.keys():
+                    contact[name]=number
+                else:
+                    newContact=input(f"there is no {name} do you want to create this contact(yes/no)").lower()
+                    if newContact=='yes':
+                        contact[name]=number
+                        print('Added Successfully')
+                    else:
+                        break
+                        
+            if choice=='delete':
+                name = input("Enter the person name:- ").capitalize()
+                if name in contact.keys():
+                    contact.remove(name)
+                    print("deleted successFully")
+                else:
+                    print("this contact is not on your app")
+                
+            if choice=='search':
+                name = input("Enter the person name:- ").capitalize()
+                if name in contact.keys():
+                    print(contact[name])
+                else:
+                    print('this is not in your contact list')
+            else:
+                for person, number in contact.items():
+                    print(f"{person}:{number}")
         newChoice = input("Do you want to exit the app (Yes/No)").lower()
         if newChoice != "no":
             break
